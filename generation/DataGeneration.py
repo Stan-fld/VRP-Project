4f8bf3fc2b99c8e0c8974944396214f37ed2562f
Bastien-reyn <50202696+Bastien-reyn@users.noreply.github.com>
2021-06-18 14:24:30 +0200

    modify generation fx: based a generation from a normal distribution, make neighbors more realistics. add warehouse for starting and loading point

--- generation/DataGeneration.py
+++ generation/DataGeneration.py
@@ -4,9 +4,11 @@ import pprint
 import numpy as np
 from sklearn.neighbors import kneighbors_graph
 from generation.Vehicle import Vehicle
+from matplotlib import pyplot as plt
 
 
 class DataGeneration:
+    warehouse = 0
     data_matrix = []
     data_vehicles: [Vehicle] = []
 
@@ -20,57 +22,65 @@ class DataGeneration:
 
     def is_graph_correct(self, max_neighbor):
         print("new graph")
+        neighbors = []
         for sommet in range(1, len(self.data_matrix) + 1):
             liste = self.data_matrix[sommet - 1]
             voisins = [i for i, value in enumerate(liste) if liste[i]]
             # test only ––––––––––––––––––––––––––––––––––––––––––––
             # display the nb neighboor
-
+            neighbors.append(len(voisins))
             print(len(voisins))
-            message = "Le sommet " + str(sommet-1) + " est de degré " + str(len(voisins)) + " (voisins : "
+            message = "Le sommet " + str(sommet - 1) + " est de degré " + str(len(voisins)) + " (voisins : "
             message += \
-            ' '.join([str(v) for v in voisins]) + ")"  #SOLUTION
+                ' '.join([str(v) for v in voisins]) + ")"  # SOLUTION
             print(message)
             # test only ––––––––––––––––––––––––––––––––––––––––––––
-            if len(voisins) < 2 or len(voisins) > max_neighbor:
-                return False
-        return True
 
-    def get_neighbors_of_summit(self, actual_summit, graph = None):
+        plt.hist(neighbors)
+        plt.title('neighbor')
+        plt.show()
+
+    def get_neighbors_of_summit(self, actual_summit, graph=None):
         graph = self.data_matrix if graph is None else graph
-        liste = graph[actual_summit] | graph[:,actual_summit]
+        liste = graph[actual_summit] | graph[:, actual_summit]
         return [i for i, value in enumerate(liste) if liste[i]]
 
     def matrix_generator(self, number_of_summit, max_neighbor):
+
+        rd = (np.random.randn(number_of_summit) * (max_neighbor / 4) + max_neighbor / 2)
+        rd = [abs(round(x, 0)) if x >= 1 else 1 for x in rd]
         graph = np.random.randint(1, size=(number_of_summit, number_of_summit))
         for i in range(number_of_summit):
-
-            dif = int(max_neighbor-len(self.get_neighbors_of_summit(i, graph)))
-            print(dif)
+            dif = int(rd[i] - len(self.get_neighbors_of_summit(i, graph)))
             if dif > 0:
                 for r in random.sample(range(0, number_of_summit), k=random.randint(1, dif)):
                     while True:
-                        print(r)
-                        print(len(self.get_neighbors_of_summit(r, graph)))
-                        if len(self.get_neighbors_of_summit(r, graph)) < max_neighbor or i == r:
+                        if len(self.get_neighbors_of_summit(r, graph)) < rd[i] and i != r:
                             graph[i][r] = 1
                             graph[r][i] = 1
                             break
                         else:
                             r = random.sample(range(0, number_of_summit), 1)[0]
         # test only ––––––––––––––––––––––––––––––––––––––––––––
+        '''
+        plt.hist(rd)
+        plt.title('rd')
+        plt.show()
         print(graph)
+        '''
         # test only ––––––––––––––––––––––––––––––––––––––––––––
         self.data_matrix = graph
 
     def __init__(self, number_of_summit, number_of_vehicle, max_neighbor):
+        self.warehouse = random.randint(0,number_of_summit)
         # generate the matrix randomly and check for constrains
         self.matrix_generator(number_of_summit, max_neighbor)
-        self.is_graph_correct(max_neighbor)
+
         # generate the vehicles
         self.vehicle_generator(number_of_vehicle)
 
         # test only ––––––––––––––––––––––––––––––––––––––––––––
+        # self.is_graph_correct(max_neighbor)
         # print(f"number of itteration to generate the graph {i}")
         # print(f"id of the {len(self.data_vehicles)} generated vehicles : ")
         # for v in self.data_vehicles:
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from matplotlib import pyplot as plt
 import networkx as nx
@@ -5,9 +7,10 @@ import networkx as nx
 from generation.DataGeneration import DataGeneration
 
 if __name__ == '__main__':
-    data = DataGeneration(number_of_summit=10, number_of_vehicle=10, max_neighbor=5)
-    print(len(data.data_vehicles))
-    #print(len(data.data_matrix))
+
+    data = DataGeneration(number_of_summit=100, number_of_vehicle=10, max_neighbor=9)
+    #print(len(data.data_vehicles))
+    # print(len(data.data_matrix))
 
     def display():
         M = np.array(data.data_matrix)
@@ -16,12 +19,13 @@ if __name__ == '__main__':
         plt.figure()
 
         options = {
-            'node_color' : 'yellow',
-            'node_size'  : 100,
-            'edge_color' : 'tab:grey',
+            'node_color': 'yellow',
+            'node_size': 100,
+            'edge_color': 'tab:grey',
             'with_labels': True
         }
         nx.draw(G2, **options)
         plt.show()
+
 
     display()
